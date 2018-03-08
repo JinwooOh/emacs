36eef3ce49ea746e9e736e38044d2bd9c60d8137
Jinwoo <ozi0414@gmail.com>
2018-03-08 14:03:59 -0600

    first commit

--- init.el
+++ init.el
@@ -1,11 +1,45 @@
+
 (setq-default inhibit-startup-screen t                        ; Skip the startup screens
               initial-scratch-message nil
-)
+	      )
+
+(setq initial-frame-alist '((left . 50) (top . 50)));;start position
+
+;;window size
+(if (display-graphic-p)
+    (progn
+      (setq initial-frame-alist
+            '(
+              (tool-bar-lines . 0)
+              (width . 130) ; chars
+              (height . 60) ; lines
+	      (top . 200) (left . 400)
+        
+              ))
+
+      (setq default-frame-alist
+            '(
+              (tool-bar-lines . 0)
+              (width . 130)
+              (height . 60)
+	      (top . 200) (left . 400)
+              )))
+  (progn
+    (setq initial-frame-alist
+          '(
+            (tool-bar-lines . 0)))
+    (setq default-frame-alist
+          '(
+            (tool-bar-lines . 0)))))
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+
 (global-linum-mode t)
 (add-hook 'after-init-hook 'global-company-mode)
+
 
 
 ;;css
@@ -22,6 +56,7 @@
 	:config
 	(which-key-mode))
 
+;;shell key setting
 (add-hook 'shell-mode-hook
   (lambda ()
     (define-key shell-mode-map (kbd "<M-up>") 'comint-previous-input)
@@ -29,6 +64,7 @@
   )
   )
 
+;;ace-window
 (use-package ace-window
 :ensure t
 :init
@@ -45,13 +81,45 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
- '(custom-enabled-themes (quote (gruber-darker)))
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
+ '(custom-enabled-themes (quote (zenburn)))
  '(custom-safe-themes
    (quote
-    ("d61fc0e6409f0c2a22e97162d7d151dee9e192a90fa623f8d6a071dbf49229c6" default)))
+    ("599f1561d84229e02807c952919cd9b0fbaa97ace123851df84806b067666332" "d61fc0e6409f0c2a22e97162d7d151dee9e192a90fa623f8d6a071dbf49229c6" default)))
+ '(fci-rule-color "#383838")
+ '(frame-brackground-mode (quote dark))
+ '(help-at-pt-display-when-idle (quote (flymake-overlay)) nil (help-at-pt))
+ '(help-at-pt-timer-delay 0.9)
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(package-selected-packages
    (quote
-    (rjsx-mode flycheck indium js-comint emmet-mode auto-complete company magit ace-window gruber-darker-theme ## which-key helm-swoop web-mode))))
+    (json-mode treemacs-projectile treemacs yasnippet zenburn-theme rjsx-mode flycheck indium js-comint emmet-mode auto-complete company magit ace-window gruber-darker-theme ## which-key helm-swoop web-mode)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -60,22 +128,96 @@
  '(web-mode-html-attr-name-face ((t (:foreground "Orange"))))
  '(web-mode-html-tag-face ((t (:foreground "systemBlueColor")))))
 
+;;rjsx
+(require 'rjsx-mode)
+(add-to-list 'auto-mode-alist '("\\.jsx$" . rjsx-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . rjsx-mode))
+
+
 ;;web-mode
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.blade\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.cmp\\'" . web-mode)) ; salesforce
+(add-to-list 'auto-mode-alist '("\\.app\\'" . web-mode)) ; salesforce
+(add-to-list 'auto-mode-alist '("\\.page\\'" . web-mode)) ; salesforce
+(add-to-list 'auto-mode-alist '("\\.component\\'" . web-mode)) ; salesforce
+(add-to-list 'auto-mode-alist '("\\.wp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tmpl\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.module\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.inc\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ftl\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jinja?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.xul?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.eex?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.xml?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mako$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.\\(jst\\|ejs\\|rhtml\\)\\(\\.erb\\)?\\'"  . web-mode)) ; ruby
+
+;;fly-check setting
+
+;; http://www.flycheck.org/manual/latest/index.html
+(require 'flycheck)
+;; (eval-after-load 'flycheck
+;;    '(flycheck-add-mode 'html-tidy 'web-mode))
+(require 'company)                                   ; load company mode
+;; turn on flychecking globally
 (add-hook 'after-init-hook #'global-flycheck-mode)
-(setq web-mode-enable-current-column-highlight t) ;;highlight column
+;; disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+	  '(javascript-jshint)))
+;; use eslint with web-mode for jsx files
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+;; customize flycheck temp file prefix
+(setq-default flycheck-temp-prefix ".flycheck")
+
+;; disable json-jsonlist checking for json files
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+	  '(json-jsonlist)))
+;; for better jsx syntax-highlighting in web-mode
+;; - courtesy of Patrick @halbtuerke
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+    (let ((web-mode-enable-part-face nil))
+      ad-do-it)
+    ad-do-it))
+
+
+
+;; you may key bind, for example for web-mode:
+(define-key web-mode-map (kbd "C-'") 'company-web-html)
+
+(defun my-web-mode-hook ()
+  "Hook for `web-mode'."
+    (set (make-local-variable 'company-backends)
+         '(company-tern company-web-html company-yasnippet company-files)))
+
+(add-hook 'web-mode-hook 'my-web-mode-hook)
+
+;; Enable JavaScript completion between <script>...</script> etc.
+(advice-add 'company-tern :before
+            #'(lambda (&rest _)
+                (if (equal major-mode 'web-mode)
+                    (let ((web-mode-cur-language
+                          (web-mode-language-at-pos)))
+                      (if (or (string= web-mode-cur-language "javascript")
+                              (string= web-mode-cur-language "jsx"))
+                          (unless tern-mode (tern-mode))
+                        (if tern-mode (tern-mode -1)))))))
+
+;;highlight column
+(setq web-mode-enable-current-column-highlight t)
+(add-hook 'web-mode-hook 'web-mode-hook-setup)
 
 (defun web-mode-indent-hook ()
   "Hooks for Web mode. Adjust indents"
@@ -85,22 +227,18 @@
 
 (add-hook 'web-mode-hook  'web-mode-indent-hook)
 (add-hook 'web-mode-hook  'emmet-mode)
+;;web mode end
 
 ;;emmet
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
- 
 (add-hook 'html-mode-hook 'emmet-mode)
+(add-hook 'rjsx-mode-hook  'emmet-mode) 
+
+
 (add-hook 'emmet-mode-hook (lambda () (setq emmet-indent-after-insert nil)))
 (add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))) ;; indent 2 spaces.
 (setq emmet-move-cursor-between-quotes t) ;; default nil
-
-;;helm
-(require 'helm-config)
-;; When doing isearch, hand the word over to helm-swoop
-(global-set-key (kbd "M-i") 'helm-swoop)
-(global-set-key (kbd "C-x b") 'helm-buffers-list)
-(global-set-key (kbd "C-x r b") 'helm-bookmarks)
 
 ;;js tern setting
 (require 'company)
@@ -109,31 +247,21 @@
 (add-hook 'js2-mode-hook (lambda ()
                            (tern-mode)
                            (company-mode)))
+;;helm
+(require 'helm-config)
+;; When doing isearch, hand the word over to helm-swoop
+(global-set-key (kbd "M-i") 'helm-swoop)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
+(global-set-key (kbd "C-x r b") 'helm-bookmarks)
 
-;;flycheck setting
-(defun flymake-html-init ()
-       (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                          'flymake-create-temp-inplace))
-              (local-file (file-relative-name
-                           temp-file
-                           (file-name-directory buffer-file-name))))
-         (list "tidy" (list local-file))))
+;;rjsx < bug
+;; @see https://github.com/felipeochoa/rjsx-mode/issues/33
+(eval-after-load 'rjsx-mode
+  '(progn
+     (define-key rjsx-mode-map "<" nil)))
 
-(defun flymake-html-load ()
-  (interactive)
-  (when (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
-    (set (make-local-variable 'flymake-allowed-file-name-masks)
-         '(("\\.html\\|\\.ctp\\|\\.ftl\\|\\.jsp\\|\\.php\\|\\.erb\\|\\.rhtml" flymake-html-init))
-         )
-    (set (make-local-variable 'flymake-err-line-patterns)
-         ;; only validate missing html tags
-         '(("line \\([0-9]+\\) column \\([0-9]+\\) - \\(Warning\\|Error\\): \\(missing <\/[a-z0-9A-Z]+>.*\\)" nil 1 2 4)))
-    (flymake-mode 1)))
-
-(defun web-mode-hook-setup ()
-  (flymake-html-load))
-(add-hook 'web-mode-hook 'web-mode-hook-setup)
-
-(custom-set-variables
-     '(help-at-pt-timer-delay 0.9)
-     '(help-at-pt-display-when-idle '(flymake-overlay)))
+;;yasnippet
+(add-to-list 'load-path
+              "~/.emacs.d/plugins/yasnippet")
+(require 'yasnippet)
+(yas-global-mode 1)
